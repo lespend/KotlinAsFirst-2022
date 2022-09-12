496a4755d659b7e94d79f7bea3e8d95115458cea
lespend <baydakov.17@yandex.ru>
2022-09-12 23:19:09 +0300

    Lesson 2 -> task 2 complete

--- test/lesson2/task2/Tests.kt
+++ test/lesson2/task2/Tests.kt
@@ -28,6 +28,7 @@ class Tests {
         assertTrue(queenThreatens(3, 6, 7, 6))
         assertTrue(queenThreatens(8, 1, 1, 8))
         assertFalse(queenThreatens(7, 6, 5, 7))
+        assertTrue(queenThreatens(2, 3, 3, 4))
     }
 
     @Test
